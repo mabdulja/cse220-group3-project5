cd1bb0ec4be74cc8a92a8ce2dbb9995ffb06ea5a
mrbrentgarber <mrbrentgarber@gmail.com>
2012-12-01 04:32:59 +0000

    fixed opening file

--- Lex.cpp
+++ Lex.cpp
@@ -108,7 +108,7 @@ std::string Lex::NextToken()
 void Lex::Reset()
 {
     Lex::mFin.close();
-    Lex::mFin.open(mSrcFname);
+    Lex::mFin.open(mSrcFname.c_str(), 'r');
 }
 
 //--------------------------------------------------------------------------------------------------------------
